8ecfdd34f7df86fa56455c4ad4f4415dc7438ae6
Victoria Usan <victoria.usan@gmail.com>
2023-03-30 18:28:46 +0200

    Better solution for 67

--- [67] Add Binary/67.add-binary.java
+++ [67] Add Binary/67.add-binary.java
@@ -12,60 +12,31 @@ import java.util.Collections;
 class Solution {
     public String addBinary(String a, String b) {
 
-        String[] strSplit1 = a.split("");
-        String[] strSplit2 = b.split("");
+        ArrayList<String> strList1 = new ArrayList<>(Arrays.asList(a.split("")));
+        ArrayList<String> strList2 = new ArrayList<>(Arrays.asList(b.split("")));
 
-        ArrayList<String> strList1 = new ArrayList<>(Arrays.asList(strSplit1));
-        ArrayList<String> strList2 = new ArrayList<>(Arrays.asList(strSplit2));
-
-        int mainNumber = 0;
-        ArrayList<String> mainList = new ArrayList<>();
-        int notMainNumber = 0;
-        ArrayList<String> notMainList = new ArrayList<>();
         ArrayList<Integer> result = new ArrayList<>();
         int increment = 0;
 
-        if (strList1.size() > strList2.size()) {
-            mainNumber = strList1.size() - 1;
-            mainList = strList1;
-            notMainNumber = strList2.size() - 1;
-            notMainList = strList2;
-        } else {
-            mainNumber = strList2.size() - 1;
-            mainList = strList2;
-            notMainNumber = strList1.size() - 1;
-            notMainList = strList1;
-        }
+        int index1 = strList1.size() - 1;
+        int index2 = strList2.size() - 1;
 
-        while (mainNumber >= 0) {
-            Integer x = Integer.valueOf(mainList.get(mainNumber));
+        while (index1 >= 0 || index2 >= 0) {
+            Integer x = index1 >= 0 ? Integer.valueOf(strList1.get(index1)) : 0;
+            Integer y = index2 >= 0 ? Integer.valueOf(strList2.get(index2)) : 0;
 
-            if (notMainNumber >= 0) {
-                Integer y = Integer.valueOf(notMainList.get(notMainNumber));
-                if (x + y == 2) {
-                    result.add(0 + increment);
-                    increment = 1;
-                } else if (x + y + increment == 2) {
-                    result.add(0);
-                    increment = 1;
-                } else {
-                    result.add(x + y + increment);
-                    increment = 0;
-                }
+            int value = x + y + increment;
+
+            if (value > 1) {
+                result.add(value % 2);
+                increment = 1;
             } else {
-                if (increment == 0) {
-                    result.add(x);
-                } else if (x + increment == 2) {
-                    result.add(0);
-                    increment = 1;
-                } else {
-                    result.add(increment);
-                    increment = 0;
-                }
+                result.add(value);
+                increment = 0;
             }
 
-            notMainNumber -= 1;
-            mainNumber -= 1;
+            index1 -= 1;
+            index2 -= 1;
         }
 
         if (increment != 0) {
@@ -83,10 +54,11 @@ class Solution {
     }
 
     // public static void main(String[] args) {
-    // var sol = new Solution();
-    // String a = "101111";
-    // String b = "10";
-    // sol.addBinary(a, b);
-    //
+    //     var sol = new Solution();
+    //     String a = "11";
+    //     String b = "1";
+    //     sol.addBinary(a, b);
+    // }
+
 }
 // @lc code=end
