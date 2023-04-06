ef16db22868a086b84aa362cf48afbbc1843f2e1
Victoria Usan <victoria.usan@gmail.com>
2023-04-06 10:55:33 +0200

    New task added

---
+++ 0094/94.binary-tree-inorder-traversal.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode id=94 lang=java
+ *
+ * [94] Binary Tree Inorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    List<Integer> result = new ArrayList<>();
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null) {
+            return result;
+        }
+        inorderTraversal(root.left);
+        result.add(root.val);
+        inorderTraversal(root.right);
+
+        return result;
+    }
+}
+// @lc code=end
